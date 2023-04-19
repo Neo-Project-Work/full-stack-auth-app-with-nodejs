9cc69f78a599260958f0ac40cd1bea0c4acb6263
Neo Jusha <neoprojectwork@gmail.com>
2023-04-19 12:33:05 +0200

    setup routes and db connection

--- src/server.js
+++ src/server.js
@@ -1,10 +1,34 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4000
+
+const dbConnect = require("./db/dbConnect");
+
+dbConnect();
+
+// Curb Cores Error by adding a header here
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Endpoint working')
+});
+
+const userRoutes = require("./routes/auth.routes")
+app.use(userRoutes );
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
